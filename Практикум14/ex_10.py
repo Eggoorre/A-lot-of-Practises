74e0e4cda58f9fda102a3655e90fea805b043077
Eggoorre <keorg25664@gmail.com>
2025-11-09 17:15:53 +0700

    Update ex_10.py

--- Практикум14/ex_10.py
+++ Практикум14/ex_10.py
@@ -6,7 +6,9 @@ nums_list_2 = list(map(int,
 range_start = int(input(f'Введите начало среза: ')) - 1
 range_end = int(input(f'Введите конец среза: '))
 
+nums_list_1_new = nums_list_1[:range_start] + nums_list_1[range_end + 1:]
+
 nums_list_2.extend(nums_list_1[range_end:range_start - 1:-1])
 
-print(f'Список 1: {nums_list_1}\n'
+print(f'Список 1: {nums_list_1_new}\n'
       f'Список 2: {nums_list_2}')
