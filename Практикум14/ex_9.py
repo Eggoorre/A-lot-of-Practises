8daa658cf26ff06feb035651e5064ed2ca4be17c
Eggoorre <keorg25664@gmail.com>
2025-11-11 09:07:54 +0700

    Update ex_9.py

--- Практикум14/ex_9.py
+++ Практикум14/ex_9.py
@@ -1,10 +1,12 @@
 stop_counter = 0
 words_list = []
 while stop_counter < 1:
-    word = input().lower().strip()
+    word = input().lower().strip(',.:-?!')
     if word == '':
         stop_counter += 1
     else:
         words_list.append(word)
 
-print(sorted(words_list, key=lambda x: words_list.count(x), reverse=True))
+new_list = sorted(words_list, key=lambda x: words_list.count(x), reverse=True)
+for word in new_list:
+    print(word)
