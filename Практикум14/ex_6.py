84a17463cfb3d59aca1d0cebf1f718f5e4784eda
Eggoorre <keorg25664@gmail.com>
2025-11-11 09:23:20 +0700

    Update ex_6.py

--- Практикум14/ex_6.py
+++ Практикум14/ex_6.py
@@ -1,9 +1,9 @@
 num = int(input())
 divs_set = set()
-for i in range(2, int((num ** 0.5)) + 1):
+for i in range(2, int((abs(num) ** 0.5)) + 1):
     divs_set.add(1)
-    if num % i == 0:
+    if abs(num) % i == 0:
         divs_set.add(i)
         divs_set.add(num // i)
-    divs_set.add(num)
+    divs_set.add(abs(num))
 print(list(divs_set))
