81a9ba5fbf01124ec6a56e0889e1aa00e7822b5f
Eggoorre <keorg25664@gmail.com>
2026-02-09 11:44:50 +0700

    Update ex_3.py

--- Практикум16/ex_3.py
+++ Практикум16/ex_3.py
@@ -1,20 +1,32 @@
-def main() -> None:
-    sl_fav_list = input(f'Введите предпочтения Сладкоежкина: ').split()
-    friends_fav_list = []
-    friends_finale = set()
+def sl_favs() -> None:
+    '''
+    function finding only Sladkoezhkins favourites
+    and printing len of set with them
+    :return: None
+    '''
+    sl_fav_set = set(input(f'Введите предпочтения Сладкоежкина: ').split())
+
+    friends_fav_set = set()
     friends_num = int(input(f'Введите кол-во друзей Сладкоежкина: '))
 
     for friend in range(friends_num):
-        friends_fav_list.append(input(f'Введите предпочтения {friend + 1}'
-                                      f'-го друга Сладкоежкина: ').split())
+        friends_fav_list = input(f'Введите предпочтения {friend + 1}'
+                                 f'-го друга Сладкоежкина: ').split()
+        for fav in friends_fav_list:
+            friends_fav_set.add(fav)
 
-    for el in friends_fav_list:
-        friends_finale.update(el)
-
-    sl_favs_ego = [x for x in sl_fav_list if x not in friends_finale]
+    sl_favs_ego = [x for x in sl_fav_set if x not in friends_fav_set]
 
     print(f'Количество наименований продуктов,'
           f' нравящихся только Сладкоежкину: {len(sl_favs_ego)}')
+
+
+def main() -> None:
+    '''
+    main function
+    :return: None
+    '''
+    sl_favs()
 
 
 if __name__ == "__main__":
