b591d9c1e8813f1d9d11957cc42748f51c73d7d4
Eggoorre <keorg25664@gmail.com>
2026-02-09 11:43:55 +0700

    Update ex_1.py

--- Практикум16/ex_1.py
+++ Практикум16/ex_1.py
@@ -1,11 +1,23 @@
-def main() -> None:
-    nums_list = list(map(int, input(f'Введите последовательность'
-                                    f' натуральных чисел: ').split()))
+def num_check() -> None:
+    '''
+    function checking if num is in input num list
+    :return: None
+    '''
+    nums = list(map(int, input(f'Введите последовательность'
+                               f' натуральных чисел: ').split()))
     num = int(input(f'Введите число для проверки: '))
-    if num in set(nums_list):
+    if num in set(nums):
         print(f'Число есть в списке')
     else:
         print(f'Числа нет в списке')
+
+
+def main() -> None:
+    '''
+    main function
+    :return: None
+    '''
+    num_check()
 
 
 if __name__ == "__main__":
