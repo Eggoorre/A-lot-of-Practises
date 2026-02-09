0b910d579d052e8f83d217821fe1b17c12e17c1a
Eggoorre <keorg25664@gmail.com>
2026-02-09 11:55:41 +0700

    Add files via upload

---
+++ Практикум16/ex_4.py
@@ -0,0 +1,31 @@
+def is_el_in() -> None:
+    '''
+    function printing answer if num is in set1 & set2
+    :return: None
+    '''
+    set_1 = set(input().split())
+    set_1_int = set(int(x) for x in set_1)
+
+    set_2 = set(input().split())
+    set_2_int = set(int(x) for x in set_2)
+
+    set_betw = set(x for x in set_1_int if x in set_2_int)
+
+    num = int(input())
+
+    if num in set_betw:
+        print(f'Значение принадлежит пересечению множеств')
+    else:
+        print(f'Значение не принадлежит пересечению множеств')
+
+
+def main() -> None:
+    '''
+    main function
+    :return: None
+    '''
+    is_el_in()
+
+
+if __name__ == "__main__":
+    main()
