04183bb043d3409e8653dad247afe77b2f64ef2d
Eggoorre <keorg25664@gmail.com>
2026-03-02 12:07:16 +0700

    Add files via upload

---
+++ Практикум17/ex_5.py
@@ -0,0 +1,39 @@
+def count_family(tree: dict, name: str) -> int:
+    '''
+    recursive function counting amount of family members
+    :param tree:
+    :param name: name of relative
+    :return: num of relatives
+    '''
+    if name not in tree:
+        return 0
+
+    total = 0
+    for child in tree[name]:
+        total += count_family(tree, child) + 1
+
+    return total
+
+
+def main():
+    '''
+    main function
+    :return:
+    '''
+    num = int(input())
+    tree = {}
+
+    for _ in range(num):
+        parent, child = input().split()
+        if parent not in tree:
+            tree[parent] = []
+
+        tree[parent].append(child)
+
+    target_mem = input()
+
+    print(count_family(tree, target_mem))
+
+
+if __name__ == '__main__':
+    main()
